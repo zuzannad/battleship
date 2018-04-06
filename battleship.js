905ed1a5b6c8d4c9f2e6dbc311c8b7b1b860358a
Zuza D <zuzanna.dermont@gmail.com>
2018-04-06 18:52:04 +0200

    add properties isSunk and fire to 'model' object

--- battleship.js
+++ battleship.js
@@ -11,16 +11,47 @@ var view = {
     var cell = document.getElementById(location);
     cell.setAttribute("class", "miss");
   }
-}
+};
 
 var model = {
-  boardSize: 7;
-  numShips: 3;
-  shipLength: 3;
-  sipsSunk: 0;
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
 
-  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""]},
-    { locations: ["24", "34", "44"], hits: ["", "", ""]},
-    { locations: ["10", "11", "12"], hits: ["", "", ""]}
-  ]
-}
+  ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i]; //pobranie okrętu
+      var index = ship.locations.indexOf(guess); //wywołanie metody indexOf
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("TRAFIONY!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("Zatopiłeś mój okręt!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("Pudło!");
+    return false;
+  },
+
+  isSunk: function(ship) {
+    for (var i=0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+model.fire("02");
+model.fire("24");
