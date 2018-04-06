3e1927bee013be717129b31741d156c4b3b70018
Zuza D <zuzanna.dermont@gmail.com>
2018-04-06 17:16:41 +0200

    add 'model' object with properties

--- battleship.js
+++ battleship.js
@@ -5,10 +5,22 @@ var view = {
   },
   displayHit: function(location) {
     var cell = document.getElementById(location);
-    cell.setAtribute("class", "hit");
+    cell.setAttribute("class", "hit");
   },
   displayMiss: function(location) {
     var cell = document.getElementById(location);
-    cell.setAtribute("class", "miss");
+    cell.setAttribute("class", "miss");
   }
+}
+
+var model = {
+  boardSize: 7;
+  numShips: 3;
+  shipLength: 3;
+  sipsSunk: 0;
+
+  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""]},
+    { locations: ["24", "34", "44"], hits: ["", "", ""]},
+    { locations: ["10", "11", "12"], hits: ["", "", ""]}
+  ]
 }
